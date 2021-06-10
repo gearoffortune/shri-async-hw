77bc278d3a2455715ab3a212cb562acc4efced9b
gearoffortune <hopelessuntildeath@gmail.com>
2021-06-10 13:19:20 +0300

    implemented traingleS that returns traingle area

--- shri-async-hw.js
+++ shri-async-hw.js
@@ -50,3 +50,52 @@
     Object.freeze(window.Homework);
 })();
 
+const promisify = (fn) => {
+    return (...args) => new Promise((resolve, reject) => {
+        try {
+            fn(...args, resolve)
+        } catch (error) {
+            reject(error)
+        }        
+    })
+}
+
+window.promisify = promisify
+async function traingleS (x1, y1, x2, y2, x3, y3){
+    const add = promisify(Homework.add);
+    const subtract = promisify(Homework.subtract);
+    const sqrt = promisify(Homework.sqrt);
+    const multiply = promisify(Homework.multiply)
+    const divide = promisify(Homework.divide);
+
+
+    async function getLength(x, y){
+        const xSq = await multiply(x, x);
+        const ySq = await multiply(y, y);
+        return add(xSq, ySq)
+        .then(l => sqrt(l))
+    }
+
+    const a = await getLength(await subtract(x1, x2), await subtract(y1, y2));
+    const b = await getLength(await subtract(x2, x3), await subtract(y2, y3));
+    const c = await getLength(await subtract(x1, x3), await subtract(y1, y3));
+
+    
+
+    const p = await divide(await add(await add(a, b), c), 2);
+    console.log({p, a, b, c})
+    return sqrt(
+        await multiply(
+            p,
+            await multiply(
+                await subtract(p, a),
+                await multiply(
+                    await subtract(p, b),
+                    await subtract(p, c)
+                )
+            )
+        )
+    )
+}
+
+window.traingleS = traingleS;
